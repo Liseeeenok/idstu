8e594a35c9f76bf52067d23afe1251a40b45c5dc
vlad.vedernikov.95@mail.ru <vlad.vedernikov.95@mail.ru>
2023-09-19 11:57:45 +0800

    Added js file and page templates

---
+++ js/main.js
@@ -0,0 +1,11 @@
+about_ins = document.getElementById('about_ins');
+
+about_ins.addEventListener('click', () => {
+    window.location = 'about_ins.html'
+})
+
+scientific_activity = document.getElementById('scientific_activity');
+
+scientific_activity.addEventListener('click', () => {
+    window.location = 'scientific_activity.html'
+})
