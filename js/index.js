b509647f598bdd2e47da6445f2374b0dccc33650
vlad.vedernikov.95@mail.ru <vlad.vedernikov.95@mail.ru>
2023-09-20 21:17:07 +0800

    Made a slider for seminars

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
-seminars_slider = document.getElementById('seminars_slider');
-seminars_slider_div = seminars_slider.getElementsByTagName('div');
-for (var i = 0; i < seminars_slider_div.length; i++) {
-    seminars_slider_div[i].addEventListener('click', (el) => {
-        for (element of seminars_slider_div) {
+seminars_slider_ellips = document.getElementById('seminars_slider_ellips');
+seminars_slider_div = seminars_slider_ellips.querySelectorAll('div');
+seminars_slider = document.getElementById("seminars_slider");
+seminars_slider_div.forEach(element => {
+    element.addEventListener('click', () => {
+        seminars_slider_div.forEach(element => {
             element.style.backgroundColor = "#ffffff";
-        }
-        el.srcElement.style.backgroundColor = "#145f9a";
-
+        });
+        element.style.backgroundColor = "#145f9a";
+        if (element.classList[0] == "ellipse_1") seminars_slider.style.cssText = 'transform: translate(0%, 0px);';
+        if (element.classList[0] == "ellipse_2") seminars_slider.style.cssText = 'transform: translate(-100%, 0px);';
+        if (element.classList[0] == "ellipse_3") seminars_slider.style.cssText = 'transform: translate(-200%, 0px);';        
     })
-}
+});
 
 publications_slider = document.getElementById('publications_slider');
 publications_slider_div = publications_slider.getElementsByTagName('div');
@@ -18,7 +21,5 @@ for (var i = 0; i < publications_slider_div.length; i++) {
             element.style.backgroundColor = "#ffffff";
         }
         el.srcElement.style.backgroundColor = "#145f9a";
-
     })
 }
-
