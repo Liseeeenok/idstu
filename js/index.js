1863ff4e4492e7007af463f83e015967a4110325
vlad.vedernikov.95@mail.ru <vlad.vedernikov.95@mail.ru>
2023-09-25 10:06:08 +0800

    Made a slider for publications

--- js/index.js
+++ js/index.js
@@ -13,8 +13,22 @@ seminars_slider_div.forEach(element => {
     })
 });
 
-publications_slider = document.getElementById('publications_slider');
-publications_slider_div = publications_slider.getElementsByTagName('div');
+publications_slider_ellips = document.getElementById('publications_slider_ellips');
+publications_slider_div = publications_slider_ellips.querySelectorAll('div');
+publications_slider = document.getElementById("publications_slider");
+publications_slider_div.forEach(element => {
+    element.addEventListener('click', () => {
+        publications_slider_div.forEach(element => {
+            element.style.backgroundColor = "#ffffff";
+        });
+        element.style.backgroundColor = "#145f9a";
+        if (element.classList[0] == "ellipse_1") publications_slider.style.cssText = 'transform: translate(0%, 0px);';
+        if (element.classList[0] == "ellipse_2") publications_slider.style.cssText = 'transform: translate(-100%, 0px);';
+        if (element.classList[0] == "ellipse_3") publications_slider.style.cssText = 'transform: translate(-200%, 0px);';        
+    })
+});
+
+
 for (var i = 0; i < publications_slider_div.length; i++) {
     publications_slider_div[i].addEventListener('click', (el) => {
         for (element of publications_slider_div) {
