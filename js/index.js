177c775d665b02719aec23efe4af045fc478c53a
Kapline <true.kapline@gmail.com>
2024-01-07 16:05:06 +0800

    Little fixes in code

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-seminars_slider_ellips = document.getElementById('seminars_slider_ellips');
+seminars_slider_ellips = document.getElementById('seminars_slider_ellipse');
 seminars_slider_div = seminars_slider_ellips.querySelectorAll('div');
 seminars_slider = document.getElementById("seminars_slider");
 seminars_slider_div.forEach(element => {
@@ -7,13 +7,13 @@ seminars_slider_div.forEach(element => {
             element.style.backgroundColor = "#ffffff";
         });
         element.style.backgroundColor = "#145f9a";
-        if (element.classList[0] == "ellipse_1") seminars_slider.style.cssText = 'transform: translate(0%, 0px);';
-        if (element.classList[0] == "ellipse_2") seminars_slider.style.cssText = 'transform: translate(-100%, 0px);';
-        if (element.classList[0] == "ellipse_3") seminars_slider.style.cssText = 'transform: translate(-200%, 0px);';        
+        if (element.classList[0] === "ellipse_1") seminars_slider.style.cssText = 'transform: translate(0%, 0px);';
+        if (element.classList[0] === "ellipse_2") seminars_slider.style.cssText = 'transform: translate(-100%, 0px);';
+        if (element.classList[0] === "ellipse_3") seminars_slider.style.cssText = 'transform: translate(-200%, 0px);';
     })
 });
 
-publications_slider_ellips = document.getElementById('publications_slider_ellips');
+publications_slider_ellips = document.getElementById('publications_slider_ellipse');
 publications_slider_div = publications_slider_ellips.querySelectorAll('div');
 publications_slider = document.getElementById("publications_slider");
 publications_slider_div.forEach(element => {
@@ -22,8 +22,8 @@ publications_slider_div.forEach(element => {
             element.style.backgroundColor = "#ffffff";
         });
         element.style.backgroundColor = "#145f9a";
-        if (element.classList[0] == "ellipse_1") publications_slider.style.cssText = 'transform: translate(0%, 0px);';
-        if (element.classList[0] == "ellipse_2") publications_slider.style.cssText = 'transform: translate(-100%, 0px);';
-        if (element.classList[0] == "ellipse_3") publications_slider.style.cssText = 'transform: translate(-200%, 0px);';        
+        if (element.classList[0] === "ellipse_1") publications_slider.style.cssText = 'transform: translate(0%, 0px);';
+        if (element.classList[0] === "ellipse_2") publications_slider.style.cssText = 'transform: translate(-100%, 0px);';
+        if (element.classList[0] === "ellipse_3") publications_slider.style.cssText = 'transform: translate(-200%, 0px);';
     })
 });
